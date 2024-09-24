cd7b30786000f50fded172ada57ace7df18f37a6
kashlya <05lozaverlan@gmail.com>
2024-09-24 11:24:28 +0200

    Добавьте файлы проекта.

---
+++ dz_praktika/Source.cpp
@@ -0,0 +1,179 @@
+﻿#include <iostream>
+using namespace std;
+
+enum valuta 
+{
+	num1 = 1,
+	num2 = 2
+};
+
+int main() {
+	/*int temperatura;
+	cout << "temperatura: ";
+	cin >> temperatura;
+	if (temperatura < 35.5)
+	{
+		cout << "u vas gipodermia";
+	}
+	if (temperatura > 35.5 && temperatura < 37)
+	{
+		cout << "vi zdorovi";
+	}
+	if (temperatura > 37 && temperatura < 41)
+	{
+		cout << "vi zaboleli ";
+	}
+	if (temperatura > 42)
+	{
+		cout << "vi umerli";
+	}*/
+
+	/*int chislo1;
+	cout << "num 1: ";
+	cin >> chislo1;
+	int chislo2;
+	cout << "num 2: ";
+	cin >> chislo2;
+	int chislo3;
+	cout << "num 3: ";
+	cin >> chislo3;
+	int min = chislo1;
+	if (chislo2 < min) min = chislo2;
+	if (chislo3 < min) min = chislo3;
+	cout << "minimum" << min << endl;
+	int max = chislo1;
+	if (chislo2 > max) max = chislo2;
+	if (chislo3 > max) max = chislo3;
+	cout << "maksimum" << max << endl;*/
+
+	/*int code;
+	double c;
+	double b;
+	cin >> code;
+	switch (code)
+	{
+	case num1:
+		double grivna;
+		cout << "grivni: ";
+		cin >> grivna;
+		c = (grivna * 41, 43);
+		cout << "grn: " << c;
+		break;
+	case num2:
+		double dollar;
+		cout << "dolari: ";
+		cin >> dollar;
+		b = (dollar * 0.024);
+		cout << "dol: " << b;
+		break;
+	default:
+		break;
+	}*/
+
+	/*int code;
+	double c;
+	double b;
+	cin >> code;
+	switch (code)
+	{
+	case num1:
+		double celsia;
+		cout << "celsia: ";
+		cin >> celsia;
+		c = (1.8 * celsia + 32);
+		cout << "F: " << c;
+		break;
+	case num2:
+		double farengeti;
+		cout << "farengeti: ";
+		cin >> farengeti;
+		b = (farengeti - 32) * 5 / 9;
+		cout << "C: " << b;
+		break;
+	default:
+		break;
+	}*/
+
+	/*int code;
+	double c;
+	double b;
+	cin >> code;
+	switch (code)
+	{
+	case num1:
+		double mili;
+		cout << "mili: ";
+		cin >> mili;
+		c = (mili * 1.609);
+		cout << "kilometri: " << c;
+		break;
+	case num2:
+		double kilometri;
+		cout << "kilometri: ";
+		cin >> kilometri;
+		b = (kilometri * 0.62);
+		cout << "mili: " << b;
+		break;
+	default:
+		break;
+	}*/
+
+    /*int code;
+	double c;
+	double b;
+	cin >> code;
+	switch (code)
+	{
+	case num1:
+		double futi;
+		cout << "futi: ";
+		cin >> futi;
+		c = (futi * 0.45);
+		cout << "kilogrami: " << c;
+		break;
+	case num2:
+		double kilogrami;
+		cout << "kilogrami: ";
+		cin >> kilogrami;
+		b = (kilogrami * 2.20);
+		cout << "futi: " << b;
+		break;
+	default:
+		break;
+	}*/
+
+    /*int code;
+	double s;
+	double a;
+	double c;
+	double b; 
+	double d;
+	cin >> code;
+	switch (code)
+	{
+	case num1:
+		cout << "a: ";
+		cin >> a;
+		cout << "b: ";
+		cin >> b;
+		s = (a + b + a + b);
+		cout << "priamougolnik: " << s;
+		break;
+	case num2:
+		cout << "a: ";
+		cin >> a;
+		cout << "b: ";
+		cin >> b;
+		cout << "c: ";
+		cin >> c;
+		d = (a + b + c);
+		cout << "treugolnik: " << d;
+		break;
+	default:
+		break;
+	}*/
+
+
+}
+
+
